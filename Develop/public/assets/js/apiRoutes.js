3873a94aa827629af4747b999b7964012fd31b44
Sabah Syed <sabisyed@gmail.com>
2020-05-14 16:43:03 -0700

    Starting fresh

--- Develop/public/assets/js/apiRoutes.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-module.exports = function(app){
-    fs.readFile("../../../db/db.json", (err,data)=>{
-        if(err) throw err;
-        let parsed = JSON.parse(data);
-        console.log(parsed);
-        console.log("JSON file has been read 1");
-    });
-}
