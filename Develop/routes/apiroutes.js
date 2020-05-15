b39cf0f3618ea7db35d11b6e85ecd771dafa3162
Sabah Syed <sabisyed@gmail.com>
2020-05-15 04:29:00 -0700

    Delete function working partially

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,31 +1,37 @@
 const fs  = require("fs");
+var updatedArray = require("../db/db.json")
 
 module.exports = function(app){
+    //SAVE a new note
     app.get("/api/notes", function(req,res){
-        console.log("Inside API");
+        console.log("***Inside SAVE ***");
         fs.readFile("./db/db.json","utf8",(err,response)=>{
             if(err){
                 throw err;
             } 
                 console.log("RESPONSE" + response);
                 let allNotes = JSON.parse(response);
-                console.log("ALL NOTES " + allNotes)
+                console.log("ALL NOTES " + allNotes);
                 res.json(allNotes);
+                
 
         });
     });
 
+
+    // POST a new note
     app.post("/api/notes", function(req,res){
-        console.log("Inside POST API" +  req.body);
+        console.log("*** Inside POST ***" +  req.body);
 
         fs.readFile("./db/db.json","utf8",(err,response)=>{
             if(err){
                 throw err;
             } 
             let allNotes = JSON.parse(response);
-            let latestID ;
+            console.log("ALL NOTES " + allNotes);
+            let latestID = 1 ;
             if (allNotes.length > 0) {
-                latestID = allNotes[allNotes.length - 1].id;
+                latestID = allNotes[allNotes.length - 1].id;  
                 latestID = latestID + 1;
               } else {
                 latestID = "1";
@@ -38,8 +44,43 @@ module.exports = function(app){
                 if(err) throw err;
                 res.json({ success: true, msg: 'Created new note' });
                 console.log("Note created!", newNote);
-            })
+            });
 
         });
     });
+
+
+    //DELETE a note
+    app.delete("/api/notes/:id" , function(req,res){
+    console.log("*** inside DELETE ***")
+    var idToDelete = req.params.id;
+    var filteredArray = [];
+    fs.readFile("./db/db.json","utf8",(err,response)=>{
+        if(err){
+            throw err;
+        } 
+    });
+
+
+     filteredArray = updatedArray.filter(function(updatedArray){
+        return updatedArray != idToDelete;
+    });
+    console.log("Filtered Array--------" +  filteredArray);
+    fs.writeFile("./db/db.json", JSON.stringify(filteredArray), err =>{
+    if(err) throw err;
+    res.json({ success: true, msg: 'Note deleted' });
+    console.log("Note deleted!", idToDelete);
+    })
+    });
 }
+
+
+// for(var i = 0;i< updatedArray.length ; i++){
+//     if(idToDelete === updatedArray[i].id)
+//     updatedArray.splice(i,1);
+// }
+// console.log(updatedArray);
+// fs.writeFile("./db/db.json", JSON.stringify(updatedArray,null,2), err =>{
+//     if(err) throw err;
+//     res.json(idToDelete);
+// });
